a9924c784e83b99f71f6bc9f84205d10d77c2cea
DJBsec <77978186+DJBsec@users.noreply.github.com>
2025-02-14 04:14:15 +0000

    Deploying to gh-pages from @ DJBsec/djbsec.github.io@6ab81df4e19108b321ed737e1674c6e1796e6a1e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -371,7 +371,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-news-for-jan-17-2025",
+          title: 'News for Jan 17 2025',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/01172025/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
