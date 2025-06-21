911735084f9547cc8dc68c672f6f08b98f81688a
DJBsec <77978186+DJBsec@users.noreply.github.com>
2025-06-21 03:09:49 +0000

    Deploying to gh-pages from @ DJBsec/djbsec.github.io@cb602bcf4cbc833d540528b18f2f8acc5533797e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blue-team/";
           },
-        },{id: "nav-red-team",
-          title: "Red Team",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/red-team/";
-          },
         },{id: "dropdown-books",
               title: "books",
               description: "",
