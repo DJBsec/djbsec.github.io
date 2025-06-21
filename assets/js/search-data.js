a39e87607b28192ff15118427037b61524652bc5
DJBsec <77978186+DJBsec@users.noreply.github.com>
2025-06-21 02:06:52 +0000

    Deploying to gh-pages from @ DJBsec/djbsec.github.io@23d3e9b2f124cd7d69be884c8a7baa2ea8bb7ccd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -351,52 +351,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-phishing",
-          title: 'Phishing',
-          description: "what is phishing?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-email-spoofing",
-          title: 'Email Spoofing',
-          description: "What is email spoofing?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/emailspoofing/";
-            },},{id: "projects-firewalls",
-          title: 'Firewalls',
-          description: "What is a firewall?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/firewalls/";
-            },},{id: "projects-ransomware",
-          title: 'Ransomware',
-          description: "What is ransomware?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/ransomware/";
-            },},{id: "projects-social-engineering",
-          title: 'Social Engineering',
-          description: "What is social engineering?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/social-engineering/";
-            },},{id: "projects-strong-passwords",
-          title: 'Strong Passwords',
-          description: "What is a strong password?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/strong-passwords/";
-            },},{id: "projects-two-factor-authentication",
-          title: 'Two-Factor Authentication',
-          description: "What is 2FA?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/two-factor-authentication/";
-            },},{id: "projects-vpn",
-          title: 'VPN',
-          description: "What is a VPN?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/vpn/";
-            },},{id: "projects-zero-trust",
-          title: 'Zero Trust',
-          description: "What is Zero Trust security?",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/zero-trust/";
-            },},{
+          section: "News",},{
         id: 'social-github',
         title: 'GitHub',
         section: 'Socials',
