30a45f9a6fe2d5d223efeb18b47f7bea77fd7472
DJBsec <77978186+DJBsec@users.noreply.github.com>
2026-05-08 11:35:53 +0000

    Deploying to gh-pages from @ DJBsec/djbsec.github.io@47ba54f88a0c2cfe1c8e28e99bdc16f0da634b62 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "post-djbsec-39-s-cybernews-2026-05-07",
+            },{id: "post-djbsec-39-s-cybernews-2026-05-08",
+      
+        title: "DJBSEC&#39;s CyberNews 2026-05-08",
+      
+      description: "2026-05-08",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2026/cybernews0508/";
+        
+      },
+    },{id: "post-djbsec-39-s-cybernews-2026-05-07",
       
         title: "DJBSEC&#39;s CyberNews 2026-05-07",
       
