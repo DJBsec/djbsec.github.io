266c4a9e8b8cd91a172585342165eb8b1c4787f8
DJBsec <77978186+DJBsec@users.noreply.github.com>
2026-05-11 11:52:19 +0000

    Deploying to gh-pages from @ DJBsec/djbsec.github.io@2070a7abb68779aebda9766b66d90a8af167a2ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -107,7 +107,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cybersecurity-calendar/";
           },
-        },{id: "post-field-notes-reading-epss-scores-in-practice",
+        },{id: "post-djbsec-39-s-cybernews-2026-05-11",
+      
+        title: "DJBSEC&#39;s CyberNews 2026-05-11",
+      
+      description: "2026-05-11",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/cybernews/2026/cybernews0511/";
+        
+      },
+    },{id: "post-field-notes-reading-epss-scores-in-practice",
       
         title: "Field Notes — Reading EPSS Scores in Practice",
       
